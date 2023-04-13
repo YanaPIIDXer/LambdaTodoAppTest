6a88c59756b188b907edc77f70bb53b3a03fb4ba
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-13 09:40:45 +0900

    REST APIのリソース構築

--- infra/lib/api/rest-api-stack.ts
+++ infra/lib/api/rest-api-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 /**
  * REST APIを構成するためのスタック
@@ -7,5 +9,17 @@ import { Construct } from 'constructs';
 export class RestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Lambda
+    const lambdaFunc = new lambda.Function(this, "lambdaFunc", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "app.lambdaHandler",
+      code: lambda.Code.fromAsset("../lambda/hello-world"),
+    });
+
+    // API Gateway
+    const apiGw = new apigateway.RestApi(this, "API");
+    const funcResource = apiGw.root.addResource("hello");
+    funcResource.addMethod("GET", new apigateway.LambdaIntegration(lambdaFunc));
   }
 };
